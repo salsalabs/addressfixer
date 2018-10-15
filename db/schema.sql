fa986abb4818dae22aa8caaec461172070369974
Allen Leonard (Salsalabs) <aleonard@salsalabs.com>
2018-10-15 15:46:28 +0000

    Added schema for MySQL/MariaDB.

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+USE addressfixer;
+
+CREATE TABLE log(at timestamp default current_timestamp,
+id varchar(32),
+city varchar(32),
+state varchar(32),
+zip varchar(32),
+country varchar(32),
+reason varchar(128)
+);
+
+CREATE TABLE postimage(at timestamp default current_timestamp,
+id varchar(32),
+city varchar(32),
+state varchar(32),
+zip varchar(32),
+country varchar(32)
+);
+
+CREATE TABLE preimage(at timestamp default current_timestamp,
+id varchar(32),
+city varchar(32),
+state varchar(32),
+zip varchar(32),
+country varchar(32)
+);
+
+CREATE UNIQUE INDEX before_id ON preimage(id);
+
+CREATE UNIQUE INDEX postimage_id ON postimage(id); 
