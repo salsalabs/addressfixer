afae9688eca4be6487836f2685e91cf9d21a6676
Allen Leonard <aleonard@salsalabs.com>
2019-02-07 13:47:42 -0600

    SQL corrections.

--- db/sqlite3_schema.sql
+++ db/sqlite3_schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE log(
-    at timestamp default current_timestamp,
-    id      integer primary key autoincrement,
+    at      timestamp default current_timestamp,
+    id      varchar(32),
     city    varchar(32),
     state   varchar(32),
     zip     varchar(32),
@@ -9,8 +9,8 @@ CREATE TABLE log(
 );
 
 CREATE TABLE postimage(
-    at timestamp default current_timestamp,
-    id      integer primary key autoincrement,
+    at      timestamp default current_timestamp,
+    id      varchar(32),
     city    varchar(32),
     state   varchar(32),
     zip     varchar(32),
@@ -18,10 +18,17 @@ CREATE TABLE postimage(
 );
 
 CREATE TABLE preimage(
-    at timestamp default current_timestamp,
-    id      integer primary key autoincrement,
+    at      timestamp default current_timestamp,
+    id      varchar(32),
     city    varchar(32),
     state   varchar(32),
     zip     varchar(32),
     country varchar(32)
 );
+
+insert into postimage(at, id) values ('1999-01-01', 1);
+CREATE UNIQUE INDEX preimage_id
+    ON preimage(id);
+
+CREATE UNIQUE INDEX postimage_id
+    ON postimage(id);
